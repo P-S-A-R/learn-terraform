b27b4276a12114a0447bafb052e05b9bb722b847
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-04-04 23:28:27 +0200

    updating ./learn-terraform

--- test/test.tf
+++ test/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
   ami                     = "ami-0089b8e98cd95257d"
   instance_type           = "t3.micro"
-  vpc_security_group_ids  = "sg-07af4adb590deb120"
+  vps_security_group_ids  = "sg-07af4adb590deb120"
   tags = {
     Name = "test"
   }
