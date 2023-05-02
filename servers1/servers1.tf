0f1a5576936ecae9d4e6c72b4603c836367bc44e
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-05-02 13:46:23 +0200

    updating ./learn-terraform

--- servers1/servers1.tf
+++ servers1/servers1.tf
@@ -1,11 +1,6 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = [" 973714476881"]
-}
 resource "aws_instance" "frontend" {
   count                   = length(var.instances)
-  ami                     = data.aws_ami.ami.image_id
+  ami                     = "ami-0b5a2b5b8f2be4ec2"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-07af4adb590deb120"]
 }
